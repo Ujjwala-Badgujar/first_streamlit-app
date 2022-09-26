c80c6694f4c1cee4f34db2543b85a3438a5b75b1
Ujjwala-Badgujar <114379746+Ujjwala-Badgujar@users.noreply.github.com>
2022-09-26 11:03:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My parents New Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Vada Sambhar')
+streamlit.text('Omlet')
+streamlit.text('Dosa')
